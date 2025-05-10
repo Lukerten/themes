9800719ae93814292566dd4b908f35b00fa19119
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-05-10 20:19:09 +0200

    feat: its actually running now

--- colorschemes/generator.nix
+++ colorschemes/generator.nix
@@ -18,16 +18,6 @@
         };
       };
     };
-
-    # Get the actual image path from the source derivation
-    # First check if the source is a hex color, then check if it's null,
-    # otherwise construct the path to the image file
-    imagePath =
-      if (isHexColor source)
-      then source
-      else if source == null
-      then "#000000" # Fallback to black when source is null
-      else "${source}/${name}.${builtins.getAttr name (lib.importJSON ../wallpapers/list.json).ext}";
   in
     pkgs.runCommand "colorscheme-${name}" {
       # __contentAddressed = true;
@@ -45,7 +35,7 @@
         if (isHexColor source)
         then "color hex"
         else "image"
-      } --config ${config} -j hex -t "scheme-$type" "${imagePath}" > "$out/$type.json"
+      } --config ${config} -j hex -t "scheme-$type" "${source}" > "$out/$type.json"
       done
     '';
 in
--- wallpapers/default.nix
+++ wallpapers/default.nix
@@ -1,43 +1,7 @@
-{pkgs}: let
-  # Create individual wallpaper derivations
-  wallpaperAttrs = pkgs.lib.listToAttrs (map (wallpaper: {
-    inherit (wallpaper) name;
-    value =
-      if wallpaper.path != null
-      then
-        pkgs.stdenv.mkDerivation {
-          name = "wallpaper-${wallpaper.name}";
-          src = ./. + "/${wallpaper.path}";
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p $out
-            cp $src $out/${wallpaper.name}
-          '';
-        }
-      else
-        # For null paths, create a minimal derivation with a fallback color
-        pkgs.runCommand "wallpaper-${wallpaper.name}" {} ''
-          mkdir -p $out
-          # Create an empty file to prevent errors
-          touch $out/${wallpaper.name}
-        '';
-  }) (pkgs.lib.importJSON ./list.json));
-
-  # Create a single derivation that collects all individual wallpapers
-  allWallpapers = pkgs.stdenv.mkDerivation {
-    name = "wallpapers";
-    dontUnpack = true;
-    buildInputs = builtins.attrValues wallpaperAttrs;
-
-    installPhase = ''
-      mkdir -p $out
-      for wallpaper in $buildInputs; do
-        for file in $(find $wallpaper -type f); do
-          cp -f $file $out/
-        done
-      done
-    '';
-  };
-in
-  # Return both the individual wallpapers and the combined collection
-  wallpaperAttrs // {default = allWallpapers;}
+{pkgs}:
+pkgs.lib.listToAttrs (map (wallpaper: {
+  inherit (wallpaper) name;
+  value = pkgs.runCommand "${wallpaper.name}.${wallpaper.ext}" {} ''
+    cp ${./images}/${wallpaper.name}.${wallpaper.ext} $out
+  '';
+}) (pkgs.lib.importJSON ./list.json))
